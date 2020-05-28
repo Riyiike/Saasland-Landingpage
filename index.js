65952f1fcd942d063a3c94bd7b1f04ac76a55f3f
Riyiike <odusanyaiyanuoluwa@gmail.com>
2020-05-28 22:08:04 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+//clicks on the icon
+function myFunction() {
+  var x = document.getElementById('mynavbar');
+  if (x.className === 'navbar') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navbar';
+  }
+}
